8825254eb9e46999d9ca75039d9e1b6388614fd1
Loriano Storchi <loriano@storchi.org>
2025-02-26 11:03:10 +0100

    to be used within python

--- fortran/equations.f90
+++ fortran/equations.f90
@@ -4,6 +4,29 @@
 
     !Gauge-dependent perturbation equations
 
+    ! Background evolution, return d tau/ d a, where tau is the conformal time
+    function dtauda(this,a)
+    use results
+    use DarkEnergyInterface
+    implicit none
+    class(CAMBdata) :: this
+    real(dl), intent(in) :: a
+    real(dl) :: dtauda, grhoa2, grhov_t
+
+    call this%CP%DarkEnergy%BackgroundDensityAndPressure(this%grhov, a, grhov_t)
+
+    !  8*pi*G*rho*a**4.
+    grhoa2 = this%grho_no_de(a) +  grhov_t * a**2
+    if (grhoa2 <= 0) then
+        call GlobalError('Universe stops expanding before today (recollapse not supported)', error_unsupported_params)
+        dtauda = 0
+    else
+        dtauda = sqrt(3 / grhoa2)
+    end if
+
+    end function dtauda
+
+
     module GaugeInterface
     use precision
     use results
