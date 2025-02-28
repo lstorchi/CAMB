2f0d23793d692657c8d3ed35dcc79f7c3bf7ff79
Marco Pezzella <marco4398@hotmail.it>
2025-02-28 12:52:23 +0100

    camb.f90 MP, 28Feb2025

--- fortran/camb.f90
+++ fortran/camb.f90
@@ -197,12 +197,31 @@
         ! Determiniamo la lunghezza totale della stringa
 
         n = size(value)
-        write(fmt,'(A,I3,A)') '(A ,',n,'(I5))'
+        write(fmt,'(A,I3,A)') '(A , A,',n,'(I5))'
         print*,"fmt",fmt
-        write(fp,fmt) trim(adjustl(name)), (value(i), i = 1,n)
+        write(fp,fmt) trim(adjustl(name)), ' = ', (value(i), i = 1,n)
     
     
     end subroutine WriteIntArray
+
+    subroutine WriteRealArray(fp, name, value)
+        implicit none
+        integer, intent(in) :: fp
+        character(len=*), intent(in) :: name   
+        real(8), dimension(:), intent(in) :: value(:)
+        character(len=:), allocatable :: str
+        integer :: i, n, total_len
+        character(len=20) :: fmt
+        ! Determiniamo la lunghezza totale della stringa
+
+        n = size(value)
+        write(fmt,'(A,I3,A)') '(A , A,',n,'(f12.6))'  ! TODO check correct formatting 
+        print*,"fmt",fmt
+        write(fp,fmt) trim(adjustl(name)),' = ' , (value(i), i = 1,n)
+    
+    
+    end subroutine WriteRealArray
+
 
     subroutine WriteBool(fp, name, value)
     integer, intent(in) :: fp
@@ -305,6 +324,7 @@
     character(len=Ini_max_string_len), intent(in) :: IniFile
     integer :: fp, num_redshiftwindows, i
     logical :: DoCounts
+    real(8) :: kmax_orig
     character(len=Ini_max_string_len) :: DarkEneryModel
     character(len=132) :: S
     character(len=7), dimension(3), parameter :: srctype=(/"21cm   ","counts ","lensing"/)
@@ -469,83 +489,58 @@
     write(fp,'(A,F10.4)') 'massless_neutrinos = ', P%Num_Nu_massless
 
     write(fp,'(A,I0)') 'nu_mass_eigenstates = ', P%Nu_mass_eigenstates
-    call WriteIntArray(fp,'massive_neutrinos = ',P%Nu_Mass_numbers(1:P%Nu_mass_eigenstates))
+    call WriteIntArray(fp,'massive_neutrinos  ',P%Nu_Mass_numbers(1:P%Nu_mass_eigenstates))
 
-!    numstr = Ini%Read_String('massive_neutrinos')
-!    read(numstr,*, iostat=status) P%Nu_Mass_numbers(1:P%Nu_mass_eigenstates)
-!    if (status/=0) then
-!        ErrMsg = 'Must give num_massive number of integer physical neutrinos for each eigenstate'
-!        return
-!    end if
-!    P%Num_Nu_massive = sum(P%Nu_Mass_numbers(1:P%Nu_mass_eigenstates))
+    if (P%Num_Nu_massive>0) then
+        print*, "P%share_delta_neff: ", P%share_delta_neff
+        call WriteBool(fp,'share_delta_neff',P%share_delta_neff)
+
+        if (P%share_delta_neff .EQV. .false. ) & 
+            call WriteRealArray(fp,'nu_mass_degeneracies ',P%Nu_mass_degeneracies(1:P%Nu_mass_eigenstates))
+
+        call WriteRealArray(fp,'nu_mass_fractions ', P%Nu_mass_fractions(1:P%Nu_mass_eigenstates))
+
+    end if 
+
+    call WriteBool(fp,'transfer_high_precision  ',P%Transfer%high_precision)
+    call WriteBool(fp,'accurate_massive_neutrino_transfers ',P%Transfer%accurate_massive_neutrinos)
+
+ ! TODO  NONLINEAR READ PARAMS  if (P%NonLinear/=NonLinear_none) call P%NonLinearModel%ReadParams(Ini)
 !
-!    if (P%Num_Nu_massive>0) then
-!        P%share_delta_neff = Ini%Read_Logical('share_delta_neff', .true.)
-!        numstr = Ini%Read_String('nu_mass_degeneracies')
-!        if (P%share_delta_neff) then
-!            if (numstr/='') write (*,*) 'WARNING: nu_mass_degeneracies ignored when share_delta_neff'
-!        else
-!            if (numstr=='') then
-!                ErrMsg = 'must give degeneracies for each eigenstate if share_delta_neff=F'
-!                return
-!            end if
-!            read(numstr,*) P%Nu_mass_degeneracies(1:P%Nu_mass_eigenstates)
-!        end if
-!        numstr = Ini%Read_String('nu_mass_fractions')
-!        if (numstr=='') then
-!            if (P%Nu_mass_eigenstates >1) then
-!                ErrMsg =  'must give nu_mass_fractions for the eigenstates'
-!                return
-!            end if
-!            P%Nu_mass_fractions(1)=1
-!        else
-!            read(numstr,*) P%Nu_mass_fractions(1:P%Nu_mass_eigenstates)
-!        end if
-!    end if
-!
-!    if (((P%NonLinear==NonLinear_lens .or. P%NonLinear==NonLinear_both) .and. P%DoLensing) .or. PK_WantTransfer) then
-!        P%Transfer%high_precision = Ini%Read_Logical('transfer_high_precision', .false.)
-!    else
-!        P%transfer%high_precision = .false.
-!    endif
-!    if (PK_WantTransfer) then
-!        P%Transfer%accurate_massive_neutrinos = Ini%Read_Logical('accurate_massive_neutrino_transfers',.false.)
-!    else
-!        P%Transfer%accurate_massive_neutrinos = .false.
-!    end if
-!    if (P%NonLinear/=NonLinear_none) call P%NonLinearModel%ReadParams(Ini)
-!
-!    if (PK_WantTransfer)  then
-!        P%WantTransfer  = .true.
-!        P%transfer%kmax = Ini%Read_Double('transfer_kmax')*(P%h0 / 100._dl)
-!        P%transfer%k_per_logint = Ini%Read_Int('transfer_k_per_logint')
-!        P%transfer%PK_num_redshifts = Ini%Read_Int('transfer_num_redshifts')
-!
-!        if (P%Do21cm) P%transfer_21cm_cl = Ini%Read_Logical('transfer_21cm_cl',.false.)
-!        if (P%transfer_21cm_cl .and. P%transfer%kmax > 800) then
-!            !Actually line widths are important at significantly larger scales too
-!            write (*,*) 'WARNING: kmax very large. '
-!            write(*,*) ' -- Neglected line width effects will dominate'
-!        end if
-!
+    if (P%WantTransfer .eqv. .true.) then
+        kmax_orig= P%transfer%kmax/(P%h0 / 100._dl) ! ASK Loriano
+         call WriteDouble(fp,'transfer_kmax ',kmax_orig)  
+        !call WriteDouble(fp,'transfer_kmax',P%transfer%kmax )  
+        ! P%transfer%kmax = Ini%Read_Double('transfer_kmax')*(P%h0 / 100._dl)
+        call WriteInteger(fp,'transfer_k_per_logint ',P%transfer%k_per_logint) 
+!        call WriteInteger(fp,'transfer_num_redshifts',P%transfer%PK_num_redshifts)
+
+        if (P%Do21cm) call WriteBool(fp,'transfer_21cm_cl ',P%transfer_21cm_cl) 
+
+!Questa parte è usata come elemtno di controllo di altri oggetti. Da quello che capisco dal codice,
+! dato che prima leggiamo e poi scriviamo, non dovremmo averne bisogno
+! Strettamente vero per transfer_interp_matterpower
+! Strettamente vero per transfer_interp_matterpower
+
 !        call Ini%Read('transfer_interp_matterpower', transfer_interp_matterpower)
 !        call Ini%Read('transfer_power_var', transfer_power_var)
 !        if (P%transfer%PK_num_redshifts > max_transfer_redshifts) then
 !            ErrMsg = 'Too many redshifts, increase max_transfer_redshifts'
 !            return
 !        end if
-!        do i=1, P%transfer%PK_num_redshifts
-!            P%transfer%PK_redshifts(i)  = Ini%Read_Double_Array('transfer_redshift', i, 0._dl)
-!        end do
-!    else
-!        P%Transfer%PK_num_redshifts = 1
-!        P%Transfer%PK_redshifts = 0
-!    end if
-!
-!    call Ini%Read('Alens', P%Alens)
-!
-!    call P%Reion%ReadParams(Ini)
-!    call P%InitPower%ReadParams(Ini)
+
+!        call WriteRealArray(fp,'transfer_redshift',P%transfer%PK_redshifts(1:P%transfer%PK_num_redshifts))
+    end if 
+
+!IN READING erando dentro input, messi fuori, dato che hanno già un valore assegnato
+    call WriteInteger(fp,'transfer_num_redshifts ',P%transfer%PK_num_redshifts)
+    call WriteRealArray(fp,'transfer_redshift ',P%transfer%PK_redshifts(1:P%transfer%PK_num_redshifts))
+
+    call WriteDouble(fp,'Alens ',P%Alens)
+! 
+!    TODO call P%Reion%ReadParams(Ini)
+!    TODO call P%InitPower%ReadParams(Ini)
+
 !
 !    RecombinationModel = UpperCase(Ini%Read_String_Default('recombination_model', 'Recfast'))
 !    if (RecombinationModel == 'COSMOREC') then
@@ -568,42 +563,37 @@
 !        ErrMsg =  'Unknown recombination_model: '//trim(RecombinationModel)
 !        return
 !    end if
+
+
+
+
 !
-!    call P%Recomb%ReadParams(Ini)
+!    TODO: call P%Recomb%ReadParams(Ini)
+
+
+    if (P%WantScalars .or. P%WantTransfer) then
+
+        call WriteInteger(fp,'initial_condition ',P%Scalar_initial_condition)
+        
+        if (P%Scalar_initial_condition == initial_vector) then
+            call WriteRealArray(fp,'initial_vector ', P%InitialConditionVector)
+        end if
+
+    end if
+
+    if (P%Scalar_initial_condition== initial_adiabatic) &
+        call WriteBool(fp,'use_cl_spline_template', P%use_cl_spline_template)
 !
-!    if (P%WantScalars .or. P%WantTransfer) then
-!        P%Scalar_initial_condition = Ini%Read_Int('initial_condition', initial_adiabatic)
-!        if (P%Scalar_initial_condition == initial_vector) then
-!            allocate(P%InitialCOnditionVector(initial_nummodes))
-!            numstr = Ini%Read_String('initial_vector', .true.)
-!            read (numstr,*) P%InitialConditionVector
-!        end if
-!        if (P%Scalar_initial_condition/= initial_adiabatic) P%use_cl_spline_template = .false.
-!    end if
-!    if (P%Scalar_initial_condition== initial_adiabatic) &
-!        call Ini%Read('use_cl_spline_template', P%use_cl_spline_template)
-!
-!    P%WantDerivedParameters = Ini%Read_Logical('derived_parameters', .true.)
-!
+    call WriteBool(fp,'derived_parameters',P%WantDerivedParameters)
+
 !    !optional parameters controlling the computation
 !
-!    P%Accuracy%AccuratePolarization = Ini%Read_Logical('accurate_polarization', .true.)
-!    P%Accuracy%AccurateReionization = Ini%Read_Logical('accurate_reionization', .true.)
-!    P%Accuracy%AccurateBB = Ini%Read_Logical('accurate_BB', .false.)
-!    if (P%Accuracy%AccurateBB .and. P%WantCls .and. (P%Max_l < 3500 .or. &
-!        (P%NonLinear/=NonLinear_lens .and. P%NonLinear/=NonLinear_both) .or. P%Max_eta_k < 18000)) &
-!        write(*,*) 'WARNING: for accurate lensing BB you need high l_max_scalar, k_eta_max_scalar and non-linear lensing'
-!
-!    !Mess here to fix typo with backwards compatibility
-!    if (Ini%HasKey('do_late_rad_trunction')) then
-!        P%DoLateRadTruncation = Ini%Read_Logical('do_late_rad_trunction', .true.)
-!        if (Ini%HasKey('do_late_rad_truncation')) error stop 'check do_late_rad_xxxx'
-!    else
-!        P%DoLateRadTruncation = Ini%Read_Logical('do_late_rad_truncation', .true.)
-!    end if
-!    P%MassiveNuMethod = Ini%Read_Int('massive_nu_approx', Nu_best)
-!
-!    call Ini%Read('l_sample_boost', P%Accuracy%lSampleBoost)
+    call WriteBool(fp,'accurate_polarization', P%Accuracy%AccuratePolarization)
+    call WriteBool(fp,'accurate_reionization',P%Accuracy%AccurateReionization)
+    call WriteBool(fp,'accurate_BB',P%Accuracy%AccurateBB)
+    call WriteBool(fp,'do_late_rad_trunction',P%DoLateRadTruncation)
+    call WriteInteger(fp,'massive_nu_approx',P%MassiveNuMethod)
+    call WriteDouble(fp,'l_sample_boost', P%Accuracy%lSampleBoost) ! USED AS INTEGER BUT DEFINED AS DOUBLE PRECISION
 
     write(fp,'(A)') '# End of parameter file'
     close(fp)
