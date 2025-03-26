024bbb41e26d54fb88b6d2a1454a5e84cae41b1c
Loriano Storchi <loriano@storchi.org>
2025-03-26 18:40:28 +0100

    start implementin the functions

--- fortran/functogpu.f90
+++ fortran/functogpu.f90
@@ -26,18 +26,37 @@ subroutine transferdata (statein, bessein)
     tau_start_redshiftwindows = statein%ThermoData%tau_start_redshiftwindows
 
     ! to copy the data 
-    !statein%TimeSteps%points
-    !statein%TimeSteps%dpoints 
-    !bessein%points
+    if (allocated(statein%TimeSteps%points)) then
+        allocate(points( size(statein%TimeSteps%points) ))
+        points = statein%TimeSteps%points
+    end if
+    if (allocated(statein%TimeSteps%dpoints)) then
+        allocate(dpoints( size(statein%TimeSteps%dpoints) ))
+        dpoints = statein%TimeSteps%dpoints
+    end if
+    if (allocated(statein%TimeSteps%points)) then
+        allocate(b_points( size(statein%TimeSteps%points) ))
+        b_points = statein%TimeSteps%points
+    end if
 
-    !functions or procedures 
-    !bessein%IndexOf  RangeUtils.f90 
-    !statein%rofChi
-    !statein%TimeSteps%IndexOf  RangeUtils.f90 procedure :: IndexOf => TRanges_IndexOf
+    print *, "tau0 = ", tau0
+    print *, "chi0 = ", chi0
+    print *, "flat = ", flat
+    print *, "closed = ", closed
+    print *, "curvature_radius = ", curvature_radius
+    print *, "num_redshiftwindows = ", num_redshiftwindows
+    print *, "num_extra_redshiftwindows = ", num_extra_redshiftwindows
+    print *, "npoints = ", npoints
+    print *, "lowest = ", lowest
+    print *, "highest = ", highest
+    print *, "tau_start_redshiftwindows = ", tau_start_redshiftwindows
+
 
 end subroutine transferdata
 
 function statindexof ()
+
+    !statein%TimeSteps%IndexOf  RangeUtils.f90 procedure :: IndexOf => TRanges_IndexOf
 
     integer :: statindexof
     
@@ -46,3 +65,33 @@ function statindexof ()
     return
 
 end function statindexof
+
+function besseindexof ()
+
+    !bessein%IndexOf  RangeUtils.f90 
+
+    integer :: besseindexof
+    
+    besseindexof = 0
+    
+    return
+
+end function besseindexof
+
+function staterofchi (flat, closed, chi) 
+    !statein%rofChi
+    logical, intent(in) :: flat, closed
+    double precision , intent(in) :: chi
+    double precision :: staterofchi
+
+    if (flat) then
+        staterofchi=chi
+    else if (closed) then
+        staterofchi=sin(chi)
+    else
+        staterofchi=sinh(chi)
+    endif
+    
+    return
+
+end function staterofchi
