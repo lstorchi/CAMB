4ba8751101f7719b21cc68b5aa0e4a95c4df6e51
Loriano Storchi <loriano@storchi.org>
2025-03-26 18:17:30 +0100

    backup

--- fortran/functogpu.f90
+++ fortran/functogpu.f90
@@ -3,11 +3,15 @@ subroutine transferdata (statein, bessein)
     use results
 
     class(CAMBdata) :: statein
-    class(TRange) :: bessein
+    class(TRanges) :: bessein
 
-    real(dl) :: tau0, chi0, curvature_radius
+    real(dl) :: tau0, chi0, curvature_radius, tau_start_redshiftwindows
     logical :: flat, closed
-    integer :: num_redshiftwindows, num_extra_redshiftwindows
+    integer :: num_redshiftwindows, num_extra_redshiftwindows, &
+        npoints
+    double precision, dimension(:), allocatable :: points, dpoints
+    double precision, dimension(:), allocatable :: b_points
+    double precision :: lowest, highest
 
     tau0 = statein%tau0
     chi0 = statein%chi0
@@ -16,21 +20,20 @@ subroutine transferdata (statein, bessein)
     curvature_radius = statein%curvature_radius
     num_redshiftwindows = statein%num_redshiftwindows
     num_extra_redshiftwindows = statein%num_extra_redshiftwindows
+    npoints = statein%TimeSteps%npoints
+    lowest = statein%TimeSteps%Lowest
+    highest = statein%TimeSteps%Highest
+    tau_start_redshiftwindows = statein%ThermoData%tau_start_redshiftwindows
 
-    !statein%TimeSteps%npoints
+    ! to copy the data 
     !statein%TimeSteps%points
-    !statein%TimeSteps%IndexOf 
     !statein%TimeSteps%dpoints 
-    !statein%TimeSteps%Highest 
-    !statein%TimeSteps%Lowest 
-    
-    !statein%ThermoData%tau_start_redshiftwindows
-
-    !bessein%IndexOf 
     !bessein%points
 
     !functions or procedures 
+    !bessein%IndexOf  RangeUtils.f90 
     !statein%rofChi
+    !statein%TimeSteps%IndexOf  RangeUtils.f90 procedure :: IndexOf => TRanges_IndexOf
 
 end subroutine transferdata
 
