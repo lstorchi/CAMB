46a8376eb41d27f016a360ffc72f2488634833b2
Loriano Storchi <loriano@storchi.org>
2025-04-24 19:51:25 +0200

    test script

--- fortran/run_comp.sh
+++ fortran/run_comp.sh
@@ -1,41 +1,42 @@
-if [not -d GPU ]; then
-  echo "GPU directory does not exist"
-  exit
-else
+if [ -d GPU ]; then
   cd GPU
   rm -f test_params.ini s_points.txt Source_q.txt s_npoints.txt iv_sourcessteps.txt ddSource_q.txt test_scalCls.dat test_scalarCovCls.dat test_lensedCls.dat test_lenspotentialCls.dat 
   cd ../
+else
+  echo "GPU directory does not exist"
+  exit
 fi
-if [not -d CPU ]; then
+if [ -d CPU ]; then
+  cd CPU
+  rm -f test_params.ini s_points.txt Source_q.txt s_npoints.txt iv_sourcessteps.txt ddSource_q.txt test_scalCls.dat test_scalarCovCls.dat test_lensedCls.dat test_lenspotentialCls.dat 
+  cd ../
+else
   echo "CPU directory does not exist"
   exit
-else
-    cd CPU
-    rm -f test_params.ini s_points.txt Source_q.txt s_npoints.txt iv_sourcessteps.txt ddSource_q.txt test_scalCls.dat test_scalarCovCls.dat test_lensedCls.dat test_lenspotentialCls.dat 
-    cd ../
 fi
 
-if [$1 == ""]; then
+if [$# -ne ""]; then
   echo "No input file provided"
   exit
 fi
 
-if [ ! -f $1 ]; then
+if [ -f $1 ]; then
+  cp ./GPU/Makefile ./
+  cd ../forutils ; make clean; cd - ; make clean ; make -j 8
+  ./camb $1 
+  mv test_params.ini s_points.txt Source_q.txt s_npoints.txt iv_sourcessteps.txt ddSource_q.txt test_scalCls.dat test_scalarCovCls.dat test_lensedCls.dat test_lenspotentialCls.dat ./GPU/
+  
+  cp ./CPU/Makefile ./
+  cd ../forutils ; make clean; cd - ; make clean ; make -j 8
+  ./camb $1 
+  mv test_params.ini s_points.txt Source_q.txt s_npoints.txt iv_sourcessteps.txt ddSource_q.txt test_scalCls.dat test_scalarCovCls.dat test_lensedCls.dat test_lenspotentialCls.dat ./CPU/
+  
+  cd CPU/
+  for name in * ; do echo $name; diff -b -B $name ../GPU/$name; done 
+  cd ../
+else
   echo "Input file does not exist"
   exit
 fi
 
 
-cp ./GPU/Makefile ./
-cd ../forutils ; make clean; cd - ; make clean ; make -j 8
-./camb $1 
-mv test_params.ini s_points.txt Source_q.txt s_npoints.txt iv_sourcessteps.txt ddSource_q.txt test_scalCls.dat test_scalarCovCls.dat test_lensedCls.dat test_lenspotentialCls.dat ./GPU/
-
-cp ./CPU/Makefile ./
-cd ../forutils ; make clean; cd - ; make clean ; make -j 8
-./camb $1 
-mv test_params.ini s_points.txt Source_q.txt s_npoints.txt iv_sourcessteps.txt ddSource_q.txt test_scalCls.dat test_scalarCovCls.dat test_lensedCls.dat test_lenspotentialCls.dat ./CPU/
-
-cd CPU/
-for name in * ; do echo $name; diff -b -B $name ../GPU/$name; done 
-cd ../
