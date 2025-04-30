edf2a6bc36d794c98c49fb57c5c46b12dfe42b76
Loriano Storchi <loriano@storchi.org>
2025-04-30 18:00:51 +0200

    backup

--- fortran/run_comp.sh
+++ fortran/run_comp.sh
@@ -21,16 +21,17 @@ if [ "$#" -ne 1 ]; then
 fi
 
 if [ -f $1 ]; then
-  cp ./GPU/Makefile ./
-  cd ../forutils ; make clean; cd - ; make clean ; make 
-  ./camb $1 
-  mv test_params.ini s_points.txt Source_q.txt s_npoints.txt iv_sourcessteps.txt ddSource_q.txt test_scalCls.dat test_scalarCovCls.dat test_lensedCls.dat test_lenspotentialCls.dat ./GPU/
-  
+ 
   cp ./CPU/Makefile ./
   cd ../forutils ; make clean; cd - ; make clean ; make 
   ./camb $1 
   mv test_params.ini s_points.txt Source_q.txt s_npoints.txt iv_sourcessteps.txt ddSource_q.txt test_scalCls.dat test_scalarCovCls.dat test_lensedCls.dat test_lenspotentialCls.dat ./CPU/
   
+  cp ./GPU/Makefile ./
+  cd ../forutils ; make clean; cd - ; make clean ; make 
+  ./camb $1 
+  mv test_params.ini s_points.txt Source_q.txt s_npoints.txt iv_sourcessteps.txt ddSource_q.txt test_scalCls.dat test_scalarCovCls.dat test_lensedCls.dat test_lenspotentialCls.dat ./GPU/
+ 
   cd CPU/
   for name in * ; do echo $name; diff -b -B $name ../GPU/$name; done 
   cd ../
