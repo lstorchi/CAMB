2743924224e58b488933afac84b929ce4f6fa00a
Loriano Storchi <loriano@storchi.org>
2025-05-19 16:12:02 +0200

    restore

--- fortran/recfast.f90
+++ fortran/recfast.f90
@@ -500,11 +500,7 @@
     use MiscUtils
     implicit none
     class(TRecfast), target :: this
-#ifndef USEACC
-    class(TCAMBdata), target :: State
-#else
     class(CAMBdata), target :: State
-#endif
     real(dl) :: Trad,Tmat,Tspin
     integer :: I
     Type(RecombinationData), pointer :: Calc
@@ -523,10 +519,8 @@
     if (.not. allocated(this%Calc)) allocate(this%Calc)
     Calc => this%Calc
 
-#ifndef USEACC
     select type(State)
       class is (CAMBdata)
-#endif
         Calc%State => State
         Calc%doTspin = DefaultFalse(WantTSpin)
 
@@ -712,11 +706,9 @@
         if (Calc%doTspin) then
             call spline_def(Calc%zrec,Calc%tsrec,nz,Calc%dtsrec)
         end if
-#ifndef USEACC
       class default
         call MpiStop('Wrong state type')
     end select
-#endif
 
     end subroutine TRecfast_init
 
